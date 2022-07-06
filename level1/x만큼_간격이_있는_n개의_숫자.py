a2e20bfc1865d0a6bcf634ba3fcb7af338706d0e
doyeon <95734836+dongnee@users.noreply.github.com>
2022-07-06 15:20:29 +0900

    Create x만큼_간격이_있는_n개의_숫자.py
    프로그래머스 lv1

---
+++ level1/x만큼_간격이_있는_n개의_숫자.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+### https://school.programmers.co.kr/learn/courses/30/lessons/12954
+
+def solution(x, n):
+    answer = []
+    
+    for i in range(n):
+        answer.append(x+x*i)
+    
+    return answer
+
